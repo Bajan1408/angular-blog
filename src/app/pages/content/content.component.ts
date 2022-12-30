cf411af58199da86ed803572f7128e0b67121a02
Bajan1408 <gianrodrigues@gmail.com>
2022-12-30 16:18:32 -0300

    Deixando a content-page dinâmica

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
+
 export class ContentComponent implements OnInit {
-  photoCover: string = 'https://miro.medium.com/max/720/0*ziNf6Ao8VbShrzHM';
-  contentHeadline:string = 'Meu Artigo';
-  contentDescription: string = 'lorem ipsum';
+  photoCover:string = '';
+  contentHeadline:string = '';
+  contentDescription:string = '';
+  private id:string | null = '0';
+  
 
 
   constructor(
     private route:ActivatedRoute
-  ) { 
-    
-  }
+  ) {  }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(value => console.log(value.get('id')));
-  }
+    this.route.paramMap.subscribe(value => this.id = value.get('id'));
 
+    this.setValuesToComponent(this.id);
+    }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0];
+
+    this.photoCover = result.photoCover;
+    this.contentHeadline = result.headline;
+    this.contentDescription = result.description;
+  }
 }
+
